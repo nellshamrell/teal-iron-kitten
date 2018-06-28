3c291d322b96bb4453692decf1298ced35bc45e9
Scott Macfarlane <macfarlane.scott@gmail.com>
2018-06-28 13:47:18 -0700

    Add four base plans for testing
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

---
+++ does-it-blend/glibc/x86_64-linux-2/plan.sh
@@ -0,0 +1,5 @@
+source "../plan.sh"
+
+pkg_version="2.22"
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="eb731406903befef1d8f878a46be75ef862b9056ab0cde1626d08a7a05328948"
---
+++ does-it-blend/linux-headers/x86_64-linux-2/plan.sh
@@ -0,0 +1,6 @@
+source "../plan.sh"
+
+pkg_version="4.11.1"
+pkg_source="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
+pkg_shasum="7e46f9e216907942b0b07f2bb59708dc33501d4e2a0938164396386ebd21e608"
+pkg_dirname="linux-$pkg_version"
